4d30a7d604b0bbfc07e2e79a6965dc8552bd3784
tyransoft <tyransoftagency@gmail.com>
2026-05-15 22:27:41 +0200

    nomm

--- project/passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()

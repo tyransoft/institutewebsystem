349e0952af541031dbc13c4571c3f2fafa39d649
tyransoft <tyransoftagency@gmail.com>
2026-06-09 18:17:17 +0200

    6789

--- project/core/templatetags/core_tags.py
+++ project/core/templatetags/core_tags.py
@@ -28,23 +28,6 @@ def has_role(user, role):
         return user.is_superuser and role == 'admin'
 
 
-@register.simple_tag
-def get_employee_attendance_status(employee, date):
-    try:
-        attendance = Attendance.objects.get(employee=employee, date=date)
-        return attendance.get_status_display()
-    except Attendance.DoesNotExist:
-        return '--'
-
-@register.simple_tag
-def get_employee_today_attendance(employee):
-    today = date.today()
-    try:
-        attendance = Attendance.objects.get(employee=employee, date=today)
-        return attendance
-    except Attendance.DoesNotExist:
-        return None
-
 @register.filter
 def payment_type_arabic(value):
     types = {
